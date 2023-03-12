2cd5c635dda9635eb2292d78e8a3609d4c7d5269
isrbrepin <isrbrepin@alum.us.es>
2023-03-12 16:53:04 +0100

    cambios en factorias

--- database/factories/SalaFactory.php
+++ database/factories/SalaFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Cita;
+use App\Models\Sala;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class CitaFactory extends Factory
+class SalaFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Cita::class;
+    protected $model = Sala::class;
 
     /**
      * Define the model's default state.
